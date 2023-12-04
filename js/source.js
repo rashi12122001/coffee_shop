1626f8163dd7b372133eb10882055d236f7e6bcf
rashirashi12122001 <rashiarora12122001@gmail.com>
2023-12-04 06:12:53 +0530

    oganisation of files

---
+++ js/source.js
@@ -0,0 +1,77 @@
+function togglemenu(){
+    const menut = document.getElementById("toggle");
+    const navigation=document.querySelector('.navigation');
+    menut.classList.toggle('active');
+    navigation.classList.toggle('active');
+    }
+    var image = document.getElementById('img');
+    function changeImage() {
+        document.getElementById("img").src="/images/strawberry_base.png";
+      }
+      function changevanilla() {
+        document.getElementById("img").src="/images/vanillla_base.png";
+      }
+      function changechoco() {
+        document.getElementById("img").src="/images/choco_base.png";
+      }
+      function changeice1() {
+  if (image.src.match("choco_base")) {
+    image.src = "/images/pink_choco.png";
+  } else if(image.src.match("vanillla_base")) {
+    image.src = "/images/pink_vanilla.png";
+  }
+  else if(image.src.match("strawberry_base")) {
+    image.src = "/images/all_pink.png";
+  }
+  else{
+    // image.src="/images/main.png";
+  }
+}
+function changeice2() {
+      if (image.src.match("choco_base")) {
+        image.src = "/images/vanilla_choco.png";
+      } else if(image.src.match("vanillla_base")) {
+        image.src = "/images/all_vanilla.png";
+      }
+      else if(image.src.match("strawberry_base")) {
+        image.src = "/images/vanilla_pink.png";
+      }
+      else{
+        // image.src="/images/main.png";
+      }
+    }
+    function changeice3() {
+          if (image.src.match("choco_base")) {
+            image.src = "/images/all_choco.png";
+          } else if(image.src.match("vanillla_base")) {
+            image.src = "/images/choco_vanilla.png";
+          }
+          else if(image.src.match("strawberry_base")) {
+            image.src = "/images/choco_pink.png";
+          }
+          else{
+            // image.src="/images/main.png";
+          }
+        }
+function reset() {
+    image.src="/images/main.png";
+  }
+  
+  const inputs = document.querySelectorAll(".inputt");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
