ab2b00e601829a05700a3ed09391c93b5f58f208
rashirashi12122001 <rashiarora12122001@gmail.com>
2024-02-13 23:14:53 +0530

    deployment issue fixed

--- js/source.js
+++ js/source.js
@@ -6,22 +6,22 @@ function togglemenu(){
     }
     var image = document.getElementById('img');
     function changeImage() {
-        document.getElementById("img").src="/images/strawberry_base.png";
+        document.getElementById("img").src="./images/strawberry_base.png";
       }
       function changevanilla() {
-        document.getElementById("img").src="/images/vanillla_base.png";
+        document.getElementById("img").src="./images/vanillla_base.png";
       }
       function changechoco() {
-        document.getElementById("img").src="/images/choco_base.png";
+        document.getElementById("img").src="./images/choco_base.png";
       }
       function changeice1() {
   if (image.src.match("choco_base")) {
-    image.src = "/images/pink_choco.png";
+    image.src = "./images/pink_choco.png";
   } else if(image.src.match("vanillla_base")) {
-    image.src = "/images/pink_vanilla.png";
+    image.src = "./images/pink_vanilla.png";
   }
   else if(image.src.match("strawberry_base")) {
-    image.src = "/images/all_pink.png";
+    image.src = "./images/all_pink.png";
   }
   else{
     // image.src="/images/main.png";
@@ -29,12 +29,12 @@ function togglemenu(){
 }
 function changeice2() {
       if (image.src.match("choco_base")) {
-        image.src = "/images/vanilla_choco.png";
+        image.src = "./images/vanilla_choco.png";
       } else if(image.src.match("vanillla_base")) {
-        image.src = "/images/all_vanilla.png";
+        image.src = "./images/all_vanilla.png";
       }
       else if(image.src.match("strawberry_base")) {
-        image.src = "/images/vanilla_pink.png";
+        image.src = "./images/vanilla_pink.png";
       }
       else{
         // image.src="/images/main.png";
@@ -42,19 +42,19 @@ function changeice2() {
     }
     function changeice3() {
           if (image.src.match("choco_base")) {
-            image.src = "/images/all_choco.png";
+            image.src = "./images/all_choco.png";
           } else if(image.src.match("vanillla_base")) {
-            image.src = "/images/choco_vanilla.png";
+            image.src = "./images/choco_vanilla.png";
           }
           else if(image.src.match("strawberry_base")) {
-            image.src = "/images/choco_pink.png";
+            image.src = "./images/choco_pink.png";
           }
           else{
             // image.src="/images/main.png";
           }
         }
 function reset() {
-    image.src="/images/main.png";
+    image.src="./images/main.png";
   }
   
   const inputs = document.querySelectorAll(".inputt");
